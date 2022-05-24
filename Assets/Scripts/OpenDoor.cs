237d3434a1d75e79ce2dab8371403c837418f6ca
BradenH <bradenhonea@yahoo.com>
2022-05-23 22:43:17 -0500

    Back to work!
    Working on this again so making repo public. And submitting some stuff

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    [SerializeField] GameObject objectToTrack;
+    private bool openDoor = false;
+    private float smooth = .01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            openDoor = true;
+        }
+        if(openDoor &&  (transform.rotation.y >= -80f))
+        {
+            Quaternion desiredRoatation = Quaternion.Euler(0, -80, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, desiredRoatation, smooth);
+        }
+        
+    }
+}
